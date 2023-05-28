3587ea0e7922a5211c2462c375a1bb9571dc1b02
Junu Park <pkjoho95@gmail.com>
2023-05-28 23:50:37 +0900

    initial commit

---
+++ src/main/kotlin/sundaystudy/kr/usedcar/UsedCarApplication.kt
@@ -0,0 +1,11 @@
+package sundaystudy.kr.usedcar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UsedCarApplication
+
+fun main(args: Array<String>) {
+	runApplication<UsedCarApplication>(*args)
+}
---
+++ src/test/kotlin/sundaystudy/kr/usedcar/UsedCarApplicationTests.kt
@@ -0,0 +1,13 @@
+package sundaystudy.kr.usedcar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UsedCarApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
