2849ace583dee46df175dd30315f55b481cb8938
ohksj77 <ohksj77@gmail.com>
2023-06-06 16:00:22 +0900

    fix used_car.sql user and add application-local.yml

--- sql/used_car.sql
+++ sql/used_car.sql
@@ -1,15 +1,20 @@
+-- DROP SCHEMA IF EXISTS `usedcar`;
+-- CREATE SCHEMA `usedcar`;
+-- USE `usedcar`;
+
 CREATE TABLE `user` (
-  `id` VARCHAR(22) PRIMARY KEY,
-  `nickname` string,
-  `manner_temperature` string,
-  `response_rate` flaot,
+  `id` bigint PRIMARY KEY,
+  `nickname` varchar(30) not null,
+  `manner_temperature` float default 36.5,
+  `rate_of_redealing` float,
+  `response_rate` float,
   `created_at` timestamp,
   `updated_at` timestamp,
   `deleted_at` timestamp
 );
 
 CREATE TABLE `bookmark_used_car_post` (
-  `id` VARCHAR(22) PRIMARY KEY,
+  `id` bigint PRIMARY KEY,
   `post_id` string,
   `followed_user_id` string,
   `created_at` timestamp,
@@ -18,7 +23,7 @@ CREATE TABLE `bookmark_used_car_post` (
 );
 
 CREATE TABLE `matching` (
-  `id` VARCHAR(22) PRIMARY KEY,
+  `id` bigint PRIMARY KEY,
   `post_id` string,
   `post_user_id` string,
   `match_request_user_id` string,
@@ -29,35 +34,40 @@ CREATE TABLE `matching` (
 );
 
 CREATE TABLE `praise` (
-  `id` VARCHAR(22) PRIMARY KEY,
-  `user_id` VARCHAR(22),
-  `praiser_id` VARCHAR(22),
-  `praiser_type` VARCHAR(22),
-  `amount` int
+  `id` bigint PRIMARY KEY,
+  `user_id` bigint,
+  `praiser_id` bigint,
+  `praise_type` varchar(50) not null,
+  `amount` int,
+  `created_at` timestamp,
+  `updated_at` timestamp,
+  `deleted_at` timestamp
 );
 
 CREATE TABLE `review` (
-  `id` VARCHAR(22) PRIMARY KEY,
-  `user_id` VARCHAR(22),
-  `reviewee` VARCHAR(22),
-  `content` TEXT
+  `id` bigint PRIMARY KEY,
+  `user_id` bigint,
+  `reviewer_id` bigint,
+  `content` TEXT,
+  `created_at` timestamp,
+  `updated_at` timestamp,
+  `deleted_at` timestamp
 );
 
 CREATE TABLE `badge` (
-  `id` VARCHAR(22) PRIMARY KEY,
-  `name` string
-);
-
-CREATE TABLE `user_badge` (
-  `id` VARCHAR(22) PRIMARY KEY,
-  `user_id` string,
-  `badge_id` string
+  `id` bigint PRIMARY KEY,
+  `user_id` bigint,
+  `badge_name` varchar(15) not null,
+  `is_represent` tinyint(1),
+  `created_at` timestamp,
+  `updated_at` timestamp,
+  `deleted_at` timestamp
 );
 
 CREATE TABLE `user_car_selling_post` (
-  `id` VARCHAR(22) PRIMARY KEY,
-  `car_id` VARCHAR(22),
-  `user_id` VARCHAR(22),
+  `id` bigint PRIMARY KEY,
+  `car_id` bigint,
+  `user_id` bigint,
   `chat` int,
   `focus` int,
   `look` int,
@@ -69,8 +79,8 @@ CREATE TABLE `user_car_selling_post` (
 );
 
 CREATE TABLE `used_car` (
-  `id` VARCHAR(22) PRIMARY KEY,
-  `car_id` VARCHAR(22),
+  `id` bigint PRIMARY KEY,
+  `car_id` bigint,
   `license_number` string,
   `price` int,
   `save_price` int,
@@ -80,7 +90,7 @@ CREATE TABLE `used_car` (
 );
 
 CREATE TABLE `car` (
-  `id` VARCHAR(22) PRIMARY KEY,
+  `id` bigint PRIMARY KEY,
   `car_type` string,
   `manufacturer` strig,
   `model_name` string,
@@ -89,11 +99,15 @@ CREATE TABLE `car` (
   `fuel_type` string
 );
 
-ALTER TABLE `user` ADD FOREIGN KEY (`id`) REFERENCES `review` (`user_id`);
+ALTER TABLE `praise` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
-ALTER TABLE `badge` ADD FOREIGN KEY (`id`) REFERENCES `user_badge` (`badge_id`);
+ALTER TABLE `praise` ADD FOREIGN KEY (`praiser_id`) REFERENCES `user` (`id`);
 
-ALTER TABLE `user_badge` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+ALTER TABLE `review` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+ALTER TABLE `review` ADD FOREIGN KEY (`reviewer_id`) REFERENCES `user` (`id`);
+
+ALTER TABLE `badge` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 ALTER TABLE `bookmark_used_car_post` ADD FOREIGN KEY (`post_id`) REFERENCES `user_car_selling_post` (`id`);
 
