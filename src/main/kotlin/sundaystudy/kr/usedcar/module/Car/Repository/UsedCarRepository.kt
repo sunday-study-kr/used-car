9294e9080071a7783f05ee10d26d946ebe172720
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2023-06-21 01:05:40 +0900

    [FEAT] 보험 , 중고차 Entity 기본 세팅

--- src/main/kotlin/sundaystudy/kr/usedcar/module/Car/Repository/UsedCarRepository.kt
+++ src/main/kotlin/sundaystudy/kr/usedcar/module/Car/Repository/UsedCarRepository.kt
@@ -1,8 +1,10 @@
 package sundaystudy.kr.usedcar.module.Car.Repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import sundaystudy.kr.usedcar.module.Car.Entity.UsedCar
 import java.util.UUID
 
+@Repository
 interface UsedCarRepository : JpaRepository<UsedCar,UUID> {
 }
--- src/main/kotlin/sundaystudy/kr/usedcar/module/Insurance/Repository/InsuranceRepository.kt
+++ src/main/kotlin/sundaystudy/kr/usedcar/module/Insurance/Repository/InsuranceRepository.kt
@@ -1,9 +1,11 @@
 package sundaystudy.kr.usedcar.module.Insurance.Repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import sundaystudy.kr.usedcar.module.Insurance.Entity.Insurance
 import java.util.*
 
+@Repository
 interface InsuranceRepository : JpaRepository<Insurance,UUID> {
     override fun findById(id: UUID): Optional<Insurance>
 }
