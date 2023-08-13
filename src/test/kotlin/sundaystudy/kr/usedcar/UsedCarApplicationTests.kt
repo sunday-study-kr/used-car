5ed4bbf176156121a5ef768519ad05f4198caa17
김승진 <ohksj77@gmail.com>
2023-08-13 16:03:31 +0900

    chore: delete custom test config class

--- src/test/kotlin/sundaystudy/kr/usedcar/UsedCarApplicationTests.kt
+++ src/test/kotlin/sundaystudy/kr/usedcar/UsedCarApplicationTests.kt
@@ -1,11 +1,11 @@
 package sundaystudy.kr.usedcar
 
 import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.ContextConfiguration
 
+@SpringBootTest
 @ActiveProfiles("test")
-@ContextConfiguration(inheritInitializers = true)
 class UsedCarApplicationTests {
 
 	@Test
